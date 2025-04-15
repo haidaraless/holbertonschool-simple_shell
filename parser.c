17a06d55b4347784f8b8380192f6357573e0c926
Haidar Alessa <10682@holbertonstudents.com>
2025-04-15 16:52:23 +0000

    Some code modificaitons to meed C90 standards.

--- parser.c
+++ parser.c
@@ -14,35 +14,44 @@
 */
 char **parse_input(char *line)
 {
-	int bufsize = 64, i = 0;
-	char **tokens = malloc(bufsize * sizeof(char *));
-	char *token;
-	char **new_tokens = realloc(tokens, bufsize * sizeof(char *));
+int bufsize = 64, i = 0;
+char **tokens = malloc(bufsize * sizeof(char *));
+char *token;
+char **new_tokens;
 
-	if (!tokens)
-	{
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
+if (!tokens)
+{
+perror("malloc");
+exit(EXIT_FAILURE);
+}
 
-	token = strtok(line, " ");
-	while (token != NULL)
-	{
-		tokens[i++] = token;
+token = strtok(line, " \t\r\n");
+while (token != NULL)
+{
+tokens[i] = strdup(token); /* Allocate and copy token */
+if (!tokens[i])
+{
+perror("strdup");
+exit(EXIT_FAILURE);
+}
 
-		if (i >= bufsize)
-		{
-			bufsize += 64;
-			if (!new_tokens)
-			{
-				free(tokens);
-				perror("realloc");
-				exit(EXIT_FAILURE);
-			}
-			tokens = new_tokens;
-		}
-		token = strtok(NULL, " ");
-	}
-	tokens[i] = NULL;
-	return (tokens);
+i++;
+
+/* Reallocate if buffer is full */
+if (i >= bufsize)
+{
+bufsize += 64;
+new_tokens = realloc(tokens, bufsize *sizeof(char *));
+if (!new_tokens)
+{
+free(tokens); /* free what you can */
+perror("realloc");
+exit(EXIT_FAILURE);
+}
+tokens = new_tokens;
+}
+token = strtok(NULL, " \t\r\n");
+}
+tokens[i] = NULL;
+return (tokens);
 }
