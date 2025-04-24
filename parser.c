bc3b1da3f555d4f1cf76fc0a243763482adb4bdd
Miad008 <10694@holbertonstudents.com>
2025-04-24 13:59:32 +0000

    parser.c

--- parser.c
+++ parser.c
@@ -3,39 +3,41 @@
 #include "simple_shell.h"
 
 /**
-* parse_input - Tokenizes a line into arguments
-* @line: user input
-* Return: NULL-terminated array of arguments
-*/
-char **parse_input(char *line)
+ * parse_line - splits a line into tokens (arguments)
+ * @line: the input line
+ *
+ * Return: array of strings (tokens)
+ */
+char **parse_line(char *line)
 {
-int bufsize = 64, i = 0;
-char **tokens = malloc(bufsize * sizeof(char *));
-char *token;
+	int bufsize = 64, i = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
 
-if (!tokens)
-{
-perror("malloc");
-exit(EXIT_FAILURE);
-}
+	if (!tokens)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
-token = strtok(line, " ");
-while (token != NULL)
-{
-tokens[i++] = token;
+	token = strtok(line, " \n\t");
+	while (token)
+	{
+		tokens[i++] = token;
+		if (i >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				perror("realloc");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, " \n\t");
+	}
+	tokens[i] = NULL;
 
-if (i >= bufsize)
-{
-bufsize += 64;
-tokens = realloc(tokens, bufsize *sizeof(char *));
-if (!tokens)
-{
-perror("realloc");
-exit(EXIT_FAILURE);
-}
-}
-token = strtok(NULL, " ");
-}
-tokens[i] = NULL;
-return (tokens);
+
+	return (tokens);
 }
