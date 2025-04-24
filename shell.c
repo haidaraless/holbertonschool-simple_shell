a85ef3851fe83dec14394642cfd0f44b0e5d98c6
Miad008 <10694@holbertonstudents.com>
2025-04-24 14:48:01 +0000

    shell.c

--- shell.c
+++ shell.c
@@ -1,9 +1,11 @@
 #include "simple_shell.h"
 #include "builtins.h"
+
 /**
-* main - Basic shell loop that handles command lines with arguments
-* Return: Always 0
-*/
+ * main - Basic shell loop that handles command lines with arguments
+ * Return: Always 0
+ */
+
 int main(void)
 {
 char *line = NULL;
@@ -12,16 +14,13 @@ ssize_t read;
 char **args;
 pid_t pid;
 int status;
-
 while (1)
 {
 if (isatty(STDIN_FILENO))
 printf("$ ");
-
 read = getline(&line, &len, stdin);
 if (read == -1)
 break;
-
 if (line[read - 1] == '\n')
 line[read - 1] = '\0';
 args = parse_input(line);
@@ -30,15 +29,16 @@ if (!args || !args[0])
 free_tokens(args);
 continue;
 }
-
-
+if (handle_builtin(args, line))
+{
+continue;
+}
 if (!check_command_in_path(args[0]))
 {
 fprintf(stderr, "%s: command not found\n", args[0]);
 free_tokens(args);
 continue;
 }
-
 pid = fork();
 if (pid == 0)
 {
@@ -52,7 +52,6 @@ else
 waitpid(pid, &status, 0);
 free_tokens(args);
 }
-
 free(line);
 return (0);
 }
