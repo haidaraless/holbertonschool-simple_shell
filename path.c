8d3a13849d902be8cc59840bea434ea0cc36933b
Miad008 <10694@holbertonstudents.com>
2025-04-24 13:30:20 +0000

    path.c

--- path.c
+++ path.c
@@ -1,132 +1,86 @@
-#include "simple_shell.h"
+#include "shell.h"
 
 /**
- * find_command - finds the full path of a command
- * @command: the command to search for
- *
- * Return: the full path to the command, or NULL if not found
- */
-
-char *find_command(char *command)
+* print_prompt - Prints the prompt
+*/
+void print_prompt(void)
 {
-char *path_env = NULL;
-char *path_copy, *dir;
-char full_path[1024];
-int i = 0;
-
-/* Search for PATH in the environment variables */
-while (environ[i])
-{
-if (strncmp(environ[i], "PATH=", 5) == 0)
-{
-path_env = environ[i] + 5;
-break;
-}
-i++;
-}
-
-if (!path_env)
-return (NULL);
-
-/* Copy the PATH and split by colon to search directories */
-path_copy = strdup(path_env);
-dir = strtok(path_copy, ":");
-
-while (dir)
-{
-snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
-if (access(full_path, X_OK) == 0)
-{
-free(path_copy);
-return (strdup(full_path));
-}
-dir = strtok(NULL, ":");
-}
-
-free(path_copy);
-return (NULL);
-}
-
-
-/**
- * execute_command - executes a command by forking a child process
- * @args: the arguments to execute
- *
- * Return: void
- */
-void execute_command(char **args)
-{
-char *command_path;
-pid_t pid;
-int status;
-
-command_path = find_command(args[0]);
-
-/* If the command doesn't exist, do not call fork */
-if (command_path == NULL)
-{
-fprintf(stderr, "Command not found: %s\n", args[0]);
-return;
-}
-
-pid = fork();
-if (pid == -1)
-{
-perror("Fork failed");
-return;
-}
-else if (pid == 0)
-{
-execve(command_path, args, environ);
-perror("Execve failed");
-exit(1);
-}
-else
-{
-wait(&status);
-}
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "($) ", 4);
 }
 
 /**
- * handle_exit - exits the shell
- *
- * Return: void
- */
-void handle_exit(void)
+* read_line - Reads a line from stdin
+* @line: Buffer to store the line
+* @len: Size of the buffer
+* Return: The number of characters read or -1 on error
+*/
+ssize_t read_line(char **line, size_t *len)
 {
-exit(0);
+return (getline(line, len, stdin));
 }
 
+/**
+* handle_exit - Handles the exit command
+* @args: Command arguments
+* @line: The input line
+* @last_status: The last exit status
+* Return: Exit status
+*/
+int handle_exit(char **args, char *line, int last_status)
+{
+int status = last_status;
+
+if (args[1])
+status = atoi(args[1]);
+
+free(line);
+free(args);
+exit(status);
+}
+
+/**
+* main - Entry point of the shell
+* Return: Always 0
+*/
 int main(void)
 {
 char *line = NULL;
 size_t len = 0;
-ssize_t nread;
+ssize_t read;
 char **args;
+int status = 0;
 
 while (1)
 {
-printf("$ ");
-nread = getline(&line, &len, stdin);
+print_prompt();
 
-if (nread == -1)
+read = read_line(&line, &len);
+if (read == -1)  /* EOF case (Ctrl+D) */
 {
-perror("getline failed");
-break;
-}
-
-line[strcspn(line, "\n")] = 0;
-
-if (strcmp(line, "exit") == 0)
-{
-handle_exit();
-}
-
-args = parse_input(line);
-execute_command(args);
-free_tokens(args);
-}
-
 free(line);
-return (0);
+exit(status);  /* Exit with the last recorded case */
+}
+
+args = parse_line(line);
+if (args[0] != NULL)
+{
+if (strcmp(args[0], "exit") == 0)
+{
+handle_exit(args, line, status);
+}
+else
+{
+status = execute_cmd(args);/* Save the exit status of the command execution */
+}
+}
+
+free(args);
+free(line); /* Freeing memory */
+line = NULL; /* Reset variable */
+len = 0; /* Reset buffer lemgth */
+}
+
+
+return (status);
 }
